01268bde93ae711b5aad8b7f1dba1b5c852ee551
Matteo Agius <mxa7743@g.coppellisd.com>
2019-06-01 20:12:29 -0500

    first commit

--- Bandz/ViewController.swift
+++ Bandz/ViewController.swift
@@ -8,7 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+@IBDesignable
+class DesignableView: UIView {
+}
+
+@IBDesignable
+class DesignableButton: UIButton {
+}
+
+@IBDesignable
+class DesignableLabel: UILabel {
+}
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }}
+
+    
+class ViewController: UIViewController {    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +72,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
